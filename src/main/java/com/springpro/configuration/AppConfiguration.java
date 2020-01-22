76043ac3eda63234ea9a40badac03a3e4293b895
Jakub Horák <jakub.horak@closeit.co>
2020-01-22 23:49:56 +0100

    Add configuration

---
+++ src/main/java/com/springpro/configuration/AppConfiguration.java
@@ -0,0 +1,65 @@
+package com.springpro.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
+
+@Slf4j
+@Configuration
+@ComponentScan(basePackages = "com.springpro")
+@EnableTransactionManagement
+public class AppConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        try {
+            EmbeddedDatabaseBuilder embeddedDatabaseBuilder = new EmbeddedDatabaseBuilder();
+            return embeddedDatabaseBuilder
+                    .setType(EmbeddedDatabaseType.H2)
+                    .addScripts("classpath:db/schema.sql", "classpath:db/test-data.sql")
+                    .build();
+        } catch (Exception e) {
+            log.error("Embedded DataSource cannot be created! " + e.getMessage());
+            return null;
+        }
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        properties.put("hibernate.format_sql", true);
+        properties.put("hibernate.use_sql_comments", true);
+        properties.put("hibernate.show_sql", true);
+        properties.put("hibernate.max_fetch_depth", 3);
+        properties.put("hibernate.jdbc.batch_size", 10);
+        properties.put("hibernate.jdbc.fetch_size", 50);
+        return properties;
+    }
+
+    @Bean
+    public SessionFactory sessionFactory() throws IOException {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan("com.springpro.entity");
+        sessionFactoryBean.setHibernateProperties(hibernateProperties());
+        sessionFactoryBean.afterPropertiesSet();
+        return sessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() throws IOException {
+        return new HibernateTransactionManager(sessionFactory());
+    }
+}
