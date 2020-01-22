38000969ddf32742d1314c5bb272fab14f484460
Jakub Horák <jakub.horak@closeit.co>
2020-01-22 21:26:48 +0100

    init

---
+++ src/main/java/com/springpro/SpringProHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.springpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProHibernateApplication.class, args);
+	}
+
+}
